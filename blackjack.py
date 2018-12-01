c32fdd76f777801ad33c5103faa7df10763710b8
mmichalke1 <36529110+mmichalke1@users.noreply.github.com>
2018-12-01 13:25:00 -0500

    Update blackjack.py
    fixed problem with Aces always being 11's. Need to finish betting mechanic and figure out suites of cards for app

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,9 @@
-import os
 import random
+import math
 
-deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']*4
+pot = 100
+bet = 0
 
 # def num_decks():
 #     print("How many decks do you want to play with?")
@@ -9,43 +11,53 @@ deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
 
 
 def deal(deck):
+    """Deals two cards and removes them from the deck"""
     hand = []
     for i in range(2):
-	    random.shuffle(deck)
-	    card = deck.pop()
-	    if card == 11:card = "Jack"
-	    if card == 12:card = "Queen"
-	    if card == 13:card = "King"
-	    if card == 14:card = "Ace"
-	    hand.append(card)
+        random.shuffle(deck)
+        card = deck.pop()
+        hand.append(card)
     return hand
 
+
 def total(hand):
+    """Sums up the total for the hand submitted"""
     total = 0
     for card in hand:
         if card is "Jack" or card is "Queen" or card is "King":
             total += 10
-        elif card is "Ace":
-            if total >= 11:
-                total += 1
-            else:
-                total += 11
-        else:
+        elif card != "Ace":
             total += card
+        else:
+            total += 11
+    if total > 21 and hand.count('Ace') > 0:
+        # an ace can be 11 or 1 so if the total is greater than 21 it should be a 1
+        total -= 10
+        if total > 21 and hand.count('Ace') > 1:
+            total -= 10
+            if total > 21 and hand.count('Ace') > 2:
+                total -= 10
+                if total > 21 and hand.count('Ace') > 3:
+                    total -= 10
+                    if total > 21 and hand.count('Ace') > 4:
+                        total -= 10
     return total
 
 
 def outcomes(dhand, phand):
-    print("dealer has" + str(dhand))
-    print("you have" + str(phand))
+    # pint("---Dealer shows " + str(dhand))
+    # print("you have" + str(phand))r
+    pass
+
 
 def score(dhand, phand):
+    """Ends the hand, tells the outcome and sets up replay"""
+    if total(dhand) == 21 and total(phand) == 21:
+        outcomes(dhand, phand)
+        print("Both you and the dealer have 21. You push.")
     if total(phand) == 21:
         outcomes(dhand, phand)
-        print("BlackJack! You Win")
-    elif total(dhand) == 21:
-        outcomes(dhand, phand)
-        print("Dealer has a BlackJack")
+        print("21! You Win")
     elif total(phand) > 21:
         outcomes(dhand, phand)
         print("You Busted. You Lose")
@@ -65,6 +77,7 @@ def score(dhand, phand):
 
 
 def blackjack(dhand, phand):
+    """Checks for Blackjack between player and dealer"""
     if total(phand) == 21:
         outcomes(dhand, phand)
         print("BlackJack You Win!")
@@ -88,14 +101,12 @@ def replay():
         exit()
 
 def options(dhand, phand):
+    """Runs player through options of game"""
     while total(phand) < 21:
         choice = input("Do you want to [Hit], or [Stay]").lower()
         if choice == "hit":
+            # if they hit, it removes a card and adds it to the hand
             card = deck.pop()
-            if card == 11:card = "Jack"
-            if card == 12:card = "Queen"
-            if card == 13:card = "King"
-            if card == 14:card = "Ace"
             phand.append(card)
             print("you now have" + str(phand) + " for a total of " + str(total(phand)))
             if total(phand) >= 21:
@@ -103,30 +114,39 @@ def options(dhand, phand):
         else:
             dealer(dhand, phand)
 
+
 def dealer(dhand, phand):
+    print("---Dealer shows " + str(dhand) + 'for a total of ' + str(total(dhand)))
     while total(dhand) < 17:
-        print("dealer hits")
+        print("---Dealer hits")
         card = deck.pop()
-        if card == 11:card = "Jack"
-        if card == 12:card = "Queen"
-        if card == 13:card = "King"
-        if card == 14:card = "Ace"
         dhand.append(card)
-        print("dealer now has" + str(dhand))
+        print("---Dealer now has " + str(dhand) + 'for a total of ' + str(total(dhand)))
     score(dhand, phand)
 
 
+def bet():
+    """Tells the player the pot and asks how much they want to bet. Need to work on verifying inputs"""
+    bet = (input('Your pot is {}. How much would you like to bet on this hand?'.format(pot)))
+    print(type(bet))
+    while type(int(bet))!= int:
+        bet = (input('Please select a number for your bet'))
+    while bet > pot:
+        bet = (input('Please select an amount less than your pot'))
+    bet = int(bet)
 
-
+    
 def game():
-	choice = 0
-	print("WELCOME TO BLACKJACK!")
-	dhand = deal(deck)
-	phand = deal(deck)
-	print("The dealer is showing a " + str(dhand[0]))
-	print("You have a " + str(phand) + " for a total of " + str(total(phand)))
-	blackjack(dhand, phand)
-	options(dhand, phand)
+    choice = 0
+    print("WELCOME TO BLACKJACK!")
+    bet()
+    dhand = deal(deck)
+    phand = deal(deck)
+    print("The dealer is showing a " + str(dhand[0]))
+    print("You have a " + str(phand) + " for a total of " + str(total(phand)))
+    blackjack(dhand, phand)
+    options(dhand, phand)
+
 
 if __name__ == "__main__":
-   game()
+    game()
